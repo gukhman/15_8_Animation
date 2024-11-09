3dc2e12ae1b400772188d3a8c7ea018ae222bcfc
gukhman <gukhman@gmail.com>
2024-11-09 21:41:36 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a15_8_animation/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.a15_8_animation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupWindowInsets(R.id.main)
+        setupToolbar(R.id.toolbar, false)
+    }
+}
